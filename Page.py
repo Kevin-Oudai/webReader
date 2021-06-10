7374c0addc7a46d747f4216868c96dd8a143b01a
Kevin-Oudai <kevinoudai@hotmail.com>
2021-06-10 19:22:39 -0400

    Update Page.py with os module

--- Page.py
+++ Page.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import os
 
 
 class Page:
@@ -63,14 +64,20 @@ class Page:
         """
         Stores the page content for the current page URL.
         """
+        path = os.path.join(os.getcwd(), 'novels', self.title)
+        try:
+            os.makedirs(path)
+        except OSError:
+            if self.output:
+                print("Folder already exists")
         if self.output:
-            filename = "chapters/{}.json".format(self.name)
+            filename = os.path.join(path, "{}.json".format(self.name))
             content = self.get_data_as_dict()
             with open(filename, 'w', encoding="utf-8") as file:
                 json.dump(content, file)
             print("{}Stored: {}".format(">" * 5, self.name))
         else:
-            filename = "chapters/{}.json".format(self.name)
+            filename = os.path.join(path, "{}.json".format(self.name))
             content = self.get_data_as_dict()
             with open(filename, 'w', encoding="utf-8") as file:
                 json.dump(content, file)
